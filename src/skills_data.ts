b52fa04dfa527e59f2ff1de45b774bbbd0fbfde3
Cyber <cyber.voyager@outlook.com>
2025-03-26 13:44:39 -0600

    Versión 0.1

--- src/skills_data.ts
+++ src/skills_data.ts
@@ -13,18 +13,13 @@ function getHTMLlayout(url: string) {
     return null;
   }
 }
-function updElement(html: string, nameClass: string, newContent: string): string {
-  const regex = new RegExp(`<([a-z]+)([^>]*)class="${nameClass}"([^>]*)>(.*?)</\\1>`, 'is');
-  return html.replace(regex, `<$1$2class="${nameClass}"$3>${newContent}</$1>`);
-}
 
 function genSkillsHTML() {
   const skillLayoutHTML = getHTMLlayout("src/skill_layout.html");
   if (!skillLayoutHTML) return;
 
   skills.forEach(skill => {
-    let newHtml = updElement(skillLayoutHTML, "skill_name", skill.name);
-    console.log(newHtml);
+    
   });
 }
 
