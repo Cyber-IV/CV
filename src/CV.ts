437f83ae0b9d4d73e35b2ac7183166480a079f58
Cyber <cyber.voyager@outlook.com>
2025-03-27 04:21:48 -0600

    Version: 0.1.1 - Se agregan elementos de skills CV.ts - Se mandan a llamar los elementos de skills skills_data.ts 1. Se recorre el arreglo de skills y se crean los elementos de la lista 2. Se agregan a un arrelgo de HTMLelements 3. Se exporta el arreglo de elementos a CV.ts package.json - Se actualiza la versión tsconfig.json - Se cambia la versión de module a ES2020 y target a ESnext

--- src/CV.ts
+++ src/CV.ts
@@ -1,6 +1,2 @@
-  /* const skillsContainer = document.getElementById('skills');
-  if (skillsContainer) {
-    skillsHTML.forEach(skill => skillsContainer.appendChild(skill));
-  } else {
-    console.error("No se encontró el contenedor con el ID 'skills container'");
-  } */
+import { skillsHTLM } from "./skills_data";
+console.log(skillsHTLM);
--- src/skills_data.ts
+++ src/skills_data.ts
@@ -1,26 +1,33 @@
-import SkillService from './services/skillsService';
-import { readFileSync } from 'fs';
+import SkillService from '../dist/services/skillsService.js';
 
 const skillService = new SkillService();
 const skills = skillService.getSkills();
 
-function getHTMLlayout(url: string) {
+async function getHTMLlayout(url: string) {
   try {
-    const response = readFileSync(url,"utf-8");
-    return response;
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Error al obtener el HTML: ${response.statusText}`);
+    }
+    return await response.text();
   } catch (error) {
     console.error('Error al obtener el HTML:', error);
     return null;
   }
 }
 
-function genSkillsHTML() {
-  const skillLayoutHTML = getHTMLlayout("src/skill_layout.html");
+let skillsHTLM: HTMLElement[] = [];
+async function genSkillsHTML() {
+  const skillLayoutHTML = await getHTMLlayout("/src/templates/skill_layout.html");
   if (!skillLayoutHTML) return;
 
   skills.forEach(skill => {
-    
+    const tempDiv: HTMLElement = document.createElement('div');
+    tempDiv.innerHTML = skillLayoutHTML;
+  
+    const skillHTML: HTMLElement = tempDiv.firstChild as HTMLElement; // Asegúrate de que sea el tipo correcto
+    skillsHTLM.push(skillHTML);
   });
 }
-
 genSkillsHTML();
+export {skillsHTLM};
