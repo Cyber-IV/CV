dacadb809e5d4e0073cbe10bfc936d3be6f3486d
Cyber <cyber.voyager@outlook.com>
2025-03-29 21:26:27 -0600

    Skills sin imagen definida pasan a ser comentadas

--- src/services/skillsService.ts
+++ src/services/skillsService.ts
@@ -7,10 +7,10 @@ class SkillService {
     { id: 3, name: 'SCSS', percentaje: 60, image: '../assets/img/sass.png' },
     { id: 4, name: 'C#', percentaje: 60, image: '../assets/img/c-sharp.png' },
     { id: 5, name: 'MySQL', percentaje: 70, image: '../assets/img/mysql.png' },
-    { id: 6, name: 'Ibexpert', percentaje: 75, image: '../assets/img/' },
+/*     { id: 6, name: 'Ibexpert', percentaje: 75, image: '../assets/img/' },
     { id: 7, name: 'Aspel SAE', percentaje: 72, image: '../assets/img/' },
     { id: 8, name: 'Aspel PROD', percentaje: 50, image: '../assets/img/' },
-  ];
+ */  ];
 
   getSkills(): Skill[] {
     return this.Skills;
