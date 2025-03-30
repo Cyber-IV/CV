52aad51ecd0581ab214013d3bfeb3906229fac61
Cyber <cyber.voyager@outlook.com>
2025-03-30 01:20:08 -0600

    Apartado de Skills terminado skill_layout.html - cambio de comillas simples a dobles skills_data.ts - generación de datos de skills y su inclusión en el CV CV.html - inclusión de skills_data.js en el CV como modulo

--- dist/skills_data.js
+++ dist/skills_data.js
@@ -14,18 +14,29 @@ async function getHTMLlayout(url) {
         return null;
     }
 }
-let skillsHTLM = [];
 async function genSkillsHTML() {
     const skillLayoutHTML = await getHTMLlayout("/src/templates/skill_layout.html");
     if (!skillLayoutHTML)
         return;
+    const parser = new DOMParser();
     skills.forEach(skill => {
-        const tempDiv = document.createElement('div');
-        tempDiv.innerHTML = skillLayoutHTML;
-        const skillHTML = tempDiv.firstChild; // Asegúrate de que sea el tipo correcto
-        skillsHTLM.push(skillHTML);
+        const doc = parser.parseFromString(skillLayoutHTML, 'text/html');
+        const skillHTML = doc.body.firstElementChild;
+        if (!skillHTML)
+            return;
+        let skillElement = skillHTML.querySelector('.skill_name');
+        if (skillElement)
+            skillElement.textContent = skill.name;
+        skillElement = skillHTML.querySelector('.skill_percent');
+        if (skillElement) {
+            skillElement.textContent = String(skill.percentaje) + '%';
+            skillElement.setAttribute('style', `width: ${skill.percentaje}%`);
+        }
+        skillElement = skillHTML.querySelector('.skill_image');
+        if (skillElement)
+            skillElement.setAttribute('src', skill.image);
+        document.querySelector('#skills')?.appendChild(skillHTML);
     });
 }
 genSkillsHTML();
-export { skillsHTLM };
 //# sourceMappingURL=skills_data.js.map
--- src/skills_data.ts
+++ src/skills_data.ts
@@ -16,18 +16,29 @@ async function getHTMLlayout(url: string) {
   }
 }
 
-let skillsHTLM: HTMLElement[] = [];
 async function genSkillsHTML() {
   const skillLayoutHTML = await getHTMLlayout("/src/templates/skill_layout.html");
   if (!skillLayoutHTML) return;
 
+  const parser = new DOMParser();
+
   skills.forEach(skill => {
-    const tempDiv: HTMLElement = document.createElement('div');
-    tempDiv.innerHTML = skillLayoutHTML;
+    const doc = parser.parseFromString(skillLayoutHTML, 'text/html');
   
-    const skillHTML: HTMLElement = tempDiv.firstChild as HTMLElement; // Asegúrate de que sea el tipo correcto
-    skillsHTLM.push(skillHTML);
+    const skillHTML = doc.body.firstElementChild as HTMLDivElement;
+    if (!skillHTML) return;
+
+    let skillElement = skillHTML.querySelector('.skill_name');
+    if (skillElement) skillElement.textContent = skill.name;
+    skillElement = skillHTML.querySelector('.skill_percent');
+    if (skillElement){
+      skillElement.textContent = String(skill.percentaje)+'%';
+      skillElement.setAttribute('style', `width: ${skill.percentaje}%`);
+    } 
+    skillElement = skillHTML.querySelector('.skill_image');
+    if (skillElement) skillElement.setAttribute('src', skill.image);
+
+    document.querySelector('#skills')?.appendChild(skillHTML);
   });
 }
 genSkillsHTML();
-export {skillsHTLM};
