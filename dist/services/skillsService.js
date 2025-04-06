69623e3257aedd8c7c4755207c0c6158ce12dc50
Cyber <cyber.voyager@outlook.com>
2025-04-06 05:49:27 -0600

    Cambio de porcentaje de habilidad HTML5 Se cambio el porcentaje de habilidad HTML5 de 75% a 87% en el archivo skillsService.ts

--- dist/services/skillsService.js
+++ dist/services/skillsService.js
@@ -1,7 +1,7 @@
 class SkillService {
     constructor() {
         this.Skills = [
-            { id: 1, name: 'HTML5', percentaje: 75, image: '../assets/img/html-5.png' },
+            { id: 1, name: 'HTML5', percentaje: 87, image: '../assets/img/html-5.png' },
             { id: 2, name: 'CSS3', percentaje: 70, image: '../assets/img/css3.png' },
             { id: 3, name: 'SCSS', percentaje: 60, image: '../assets/img/sass.png' },
             { id: 4, name: 'C#', percentaje: 60, image: '../assets/img/c-sharp.png' },
--- src/services/skillsService.ts
+++ src/services/skillsService.ts
@@ -2,7 +2,7 @@ import Skill from '../models/skills';
 
 class SkillService {
   private Skills: Skill[] = [
-    { id: 1, name: 'HTML5', percentaje: 75, image: '../assets/img/html-5.png' },
+    { id: 1, name: 'HTML5', percentaje: 87, image: '../assets/img/html-5.png' },
     { id: 2, name: 'CSS3', percentaje: 70, image: '../assets/img/css3.png' },
     { id: 3, name: 'SCSS', percentaje: 60, image: '../assets/img/sass.png' },
     { id: 4, name: 'C#', percentaje: 60, image: '../assets/img/c-sharp.png' },
