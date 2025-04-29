45b3d51cdcac80f3701f49e2a798acb5841024d1
Cyber <cyber.voyager@outlook.com>
2025-04-29 10:32:15 -0600

    Traduccion de espanol a ingles y de regreso

--- src/services/experienceService.ts
+++ src/services/experienceService.ts
@@ -5,13 +5,13 @@ class ExperienceService {
         {
             id: 1,
             position: 'Desarrollador Software & Full Stack',
-            company: 'Industrias Dabe S.A. de C.V.',
+            company: 'Grupo Dabe S.A. de C.V.',
             location: 'Monterrey, N.L., México',
             startDate: new Date('2022-04-01'),
             endDate: new Date('2023-08-01'),
             responsabilities: [
-                { description: 'Creación de paginas web y gestión de e-commerce' },
-                { description: 'Creación de aplicaciones para externer funciones del ERP Aspel SAE & PROD' },
+                { description: 'Creación de páginas web y gestión de e-commerce' },
+                { description: 'Creación de aplicaciones para extender funciones del ERP Aspel SAE & PROD' },
                 { description: 'Creación de reportes de finanzas e inventarios mediante consultas' },
             ],
         },
@@ -23,7 +23,7 @@ class ExperienceService {
             startDate: new Date('2020-04-01'),
             endDate: new Date('2022-04-01'),
             responsabilities: [
-                { description: 'Creación de aplicaciónes de escritorio y automatización para mejora de procesos internos' },
+                { description: 'Creación de aplicaciones de escritorio y automatización para mejora de procesos internos' },
                 { description: 'Ampliación y automatización de funciones del ERP Aspel SAE & PROD' },
                 { description: 'Desarrollo y consumo de APIs RESTful en formato JSON' },
             ],
@@ -36,7 +36,7 @@ class ExperienceService {
             startDate: new Date('2019-06-01'),
             endDate: new Date('2019-09-01'),
             responsabilities: [
-                { description: 'Mantenimiento de paginas web para equipos locales de baloncesto' },
+                { description: 'Mantenimiento de páginas web para equipos locales de baloncesto' },
                 { description: 'Consumo e implementación de APIs' },
             ],
         },
