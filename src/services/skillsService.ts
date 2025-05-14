b655b2fc226cb22ab1957e19a84d09edeb545402
Cyber <cyber.voyager@outlook.com>
2025-05-13 21:32:01 -0600

    Adding SAE & PROD  to the skills learned

--- src/services/skillsService.ts
+++ src/services/skillsService.ts
@@ -8,10 +8,10 @@ class SkillService {
     { id: 4, name: 'SCSS', percentaje: 60, image: '../assets/img/sass.png' },
     { id: 5, name: 'C#', percentaje: 60, image: '../assets/img/c-sharp.png' },
     { id: 6, name: 'MySQL', percentaje: 70, image: '../assets/img/mysql.png' },
-/*  { id: 7, name: 'Ibexpert', percentaje: 75, image: '../assets/img/' },
-    { id: 8, name: 'Aspel SAE', percentaje: 72, image: '../assets/img/' },
-    { id: 9, name: 'Aspel PROD', percentaje: 50, image: '../assets/img/' },
- */  ];
+/*  { id: 7, name: 'Ibexpert', percentaje: 75, image: '../assets/img/' },*/
+    { id: 8, name: 'Aspel SAE', percentaje: 70, image: '../assets/img/skill-placeholder.png' },
+    { id: 9, name: 'Aspel PROD', percentaje: 50, image: '../assets/img/skill-placeholder.png' },
+  ];
 
   getSkills(): Skill[] {
     return this.Skills;
